511e8e5725c0e498db825c4df2c97ee4c1e69e61
BananaAppleCoder <mohapatra9@gmail.com>
2021-06-26 12:14:29 -0500

    broken

--- Rubber.js
+++ Rubber.js
@@ -1,7 +1,13 @@
 class Rubber{
-	constructor(x,y,r)
-	{
+	constructor(x,y,r){
 	// assign options to the rubber ball
+		const options = {
+			"restitution":0.8,
+			"friction":5,
+			"density":12,
+			"isStatic":true
+		}
+
 		this.x=x;
 		this.y=y;
 		this.r=r
@@ -9,17 +15,20 @@ class Rubber{
 		World.add(world, this.body);
 
 	}
-	display()
-	{
-			var rubberpos=this.body.position;		
+	display(){
+			var rubberpos=this.body.position;	
+			var angle = this.body.angle;
+
 			push()
 			translate(rubberpos.x, rubberpos.y);
+			rotate(angle)
 			rectMode(CENTER)
 			strokeWeight(4);
 			stroke("black");
 			fill("darkblue");
 			//draw the ellipse here to display the rubber ball
-
+			(rubberpos.x, rubberpos.y, this.r, this.r)
+			console.log(this.body)
 			pop()
 	}
 
--- sketch.js
+++ sketch.js
@@ -3,30 +3,25 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var hammer;
+var hammer, plane, ball;
 
 
 function setup(){
-    var canvas = createCanvas(1200,600);
+    var canvas = createCanvas(windowWidth,windowHeight);
     engine = Engine.create();
     world = engine.world;
 
     plane = new Plane(600,height,1200,20)
     hammer = new Hammer(10,100);
-
-  
-
-
+    ball = new Rubber(300, 600, 100);
 }
 
 function draw(){
-    background("lightBlue");
+    //background("lightBlue");
     Engine.update(engine);
 
 
     plane.display();
     hammer.display();
-
-    
- 
+    ball.display();
 }
